e66043ec93c0a48c02febaf6ce4a36109e1f26b9
bacuda <appxcode450@gmail.com>
2023-04-07 16:11:57 +0700

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Bakhtovar Akhmedov on 07.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
